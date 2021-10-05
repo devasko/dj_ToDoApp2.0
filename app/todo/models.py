c8ddb4fb62c3a6ad4ddcc714780e48a8318e4a93
Alex <Devasko@ro.ru>
2021-10-05 22:03:31 +0300

    All app files

--- app/todo/models.py
+++ app/todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Задача')
+    complete = models.BooleanField(default=False, verbose_name='Завершено?')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Записано')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
--- app/todo/views.py
+++ app/todo/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import *
+from .forms import *
+
+
+def index(request):
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'todo/list.html', context)
+
+
+def update(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+
+    return render(request, 'todo/update.html', context)
+
+
+def delete(request, pk):
+    task = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+
+    context = {'task': task}
+    return render(request, 'todo/delete.html', context)
